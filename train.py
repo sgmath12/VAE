e3a1ca6365ed4ef4509d39bd397843509c07fe2a
seungju <sgmath12@gmail.com>
2020-11-19 13:03:18 +0900

    ...

--- train.py
+++ train.py
@@ -14,18 +14,18 @@ transform = transforms.Compose(
 
 path = os.path.expanduser( '~/data/MNIST')
 model_save_path = os.path.expanduser('./saved_models')
-trainset = torchvision.datasets.MNIST(root = path, train=False,
-                                        download=True, transform=transform)
+trainset = torchvision.datasets.MNIST(root = path, train=True,
+                                        download=False, transform=transform)
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size= 16,shuffle=True)
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+trainLoader = torch.utils.data.DataLoader(trainset, batch_size= 16,shuffle=True)
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
 model = vae_MNIST()
 model = model.to(device)
 optimizer = optim.Adam(model.parameters(), lr = 0.0001)
-
-for epoch in range(30):
+for epoch in range(10):
     running_loss = 0.0
-    for i,(x,y) in tqdm(enumerate(trainloader)):
+    for i,(x,y) in tqdm(enumerate(trainLoader)):
         x,y = x.to(device),y.to(device)
         optimizer.zero_grad()
         mu,log_var,out = model(x)
@@ -36,6 +36,6 @@ for epoch in range(30):
             print(loss.item())
 
 # save the model
-torch.save(model.state_dict(),model_save_path+'model.pth')
+torch.save(model.state_dict(),model_save_path+'/model.pth')
 print ("-"*15)
 print ("End Training")
